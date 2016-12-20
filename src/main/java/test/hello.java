2d7a5b3315df9b4a90eeaa67bcc2c4448c6f3709
Wanghao <Wanghao@epsr>
2016-12-20 10:04:28 +0800

    finish test hello

--- src/main/java/test/hello.java
+++ src/main/java/test/hello.java
@@ -2,17 +2,20 @@ package test;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 @WebServlet(name="hello",urlPatterns="/hello")
 public class hello extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// 向浏览器中写出hello world
 		resp.setContentType("text/html;charSet=utf-8");
         PrintWriter out = resp.getWriter();
         out.print("hello world");
--- src/main/test/test/helloTest.java
+++ src/main/test/test/helloTest.java
@@ -1,19 +1,26 @@
 package test;
 
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.meterware.httpunit.GetMethodWebRequest;
+import com.meterware.httpunit.WebConversation;
+import com.meterware.httpunit.WebRequest;
+import com.meterware.httpunit.WebResponse;
 import com.meterware.servletunit.ServletRunner;
+import com.meterware.servletunit.ServletUnitClient;
 
 import junit.framework.TestCase;
 
-public class helloTest extends TestCase  {
+public class helloTest extends TestCase {
 
 	@Before
 	public void setUp() throws Exception {
 		super.setUp();
 	}
+
 	@After
 	public void tearDown() throws Exception {
 		super.tearDown();
@@ -21,7 +28,19 @@ public class helloTest extends TestCase  {
 
 	@Test
 	public void testDoGetHttpServletRequestHttpServletResponse() {
-		ServletRunner sr = new ServletRunner();
+		try {
+			// 创建Servlet的运行环境
+			ServletRunner servletRunner = new ServletRunner();
+			servletRunner.registerServlet("hello", hello.class.getName());
+			ServletUnitClient servletUnitClient = servletRunner.newClient();
+
+			// 模仿用户访问http://localhost:8080/test/hello
+			WebRequest webRequest = new GetMethodWebRequest("http://localhost:8080/test/hello");
+			WebResponse webResponse = servletUnitClient.getResource(webRequest);
+			Assert.assertTrue(webResponse.getText().equals("hello world"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
